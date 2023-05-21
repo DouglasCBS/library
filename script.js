609ac15fc3ed2644401cfa68b3e391514aa6f290
DouglasCBS <douglascesar12@gmail.com>
2023-05-20 22:04:57 -0300

    complete basic code ready for any changes

---
+++ script.js
@@ -0,0 +1,29 @@
+let myLibrary = []
+
+function Book(title, author, numberOfPages, status) {
+    this.title = title
+    this.author = author
+    this.numberOfPages = numberOfPages
+    this.status = status
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+}
+
+// temporary code to better see array
+
+const waves = new Book("waves", "ted", 10, "reading")
+const princes = new Book("princes", "jonas", 30, "dropped")
+
+addBookToLibrary(waves)
+addBookToLibrary(princes)
+
+
+// end of temporary code
+
+function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log(myLibrary[i])
+    }
+}
