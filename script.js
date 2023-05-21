d22b800ce152ad6971b236da55cba57285dca969
DouglasCBS <douglascesar12@gmail.com>
2023-05-20 23:41:59 -0300

    Add semicolons and dummy css

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-let myLibrary = []
+let myLibrary = [];
+
+const contentBody = document.querySelector("#contentBody");
 
 function Book(title, author, numberOfPages, status) {
     this.title = title
     this.author = author
     this.numberOfPages = numberOfPages
     this.status = status
-}
+};
 
 function addBookToLibrary(book) {
     myLibrary.push(book)
-}
+};
 
 // temporary code to better see array
 
-const waves = new Book("waves", "ted", 10, "reading")
-const princes = new Book("princes", "jonas", 30, "dropped")
+const waves = new Book("waves", "ted", 10, "reading");
+const princes = new Book("princes", "jonas", 30, "dropped");
 
-addBookToLibrary(waves)
-addBookToLibrary(princes)
+addBookToLibrary(waves);
+addBookToLibrary(princes);
 
 
 // end of temporary code
@@ -26,4 +28,4 @@ function displayBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
         console.log(myLibrary[i])
     }
-}
+};
