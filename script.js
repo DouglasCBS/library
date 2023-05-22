6a1de59ee7dc7a5205303b0c150aafb70c20074d
DouglasCBS <douglascesar12@gmail.com>
2023-05-21 21:37:32 -0300

    create form to input new books

--- script.js
+++ script.js
@@ -35,7 +35,89 @@ function displayBooks() {
     }
 };
 
-displayBooks()
+displayBooks();
+
+newBook.addEventListener("click", () => console.log(this));
+
+function openForm() {
+    const bookForm = document.createElement("form");
+    bookForm.setAttribute("action", ".");
+    bookForm.setAttribute("method", "post");
+    contentBody.appendChild(bookForm);
+    
+    const LabelTitle = document.createElement("label");
+    LabelTitle.setAttribute("for", "title");
+    LabelTitle.textContent = "Title: ";
+    bookForm.appendChild(LabelTitle);
+    const InputTitle = document.createElement("input");
+    InputTitle.setAttribute("type", "text");
+    InputTitle.setAttribute("id", "title");
+    InputTitle.setAttribute("required", "");
+    bookForm.appendChild(InputTitle);
+
+    const LabelAuthor = document.createElement("label");
+    LabelAuthor.setAttribute("for", "author");
+    LabelAuthor.textContent = "Author: ";
+    bookForm.appendChild(LabelAuthor);
+    const InputAuthor = document.createElement("input");
+    InputAuthor.setAttribute("type", "text");
+    InputAuthor.setAttribute("id", "author");
+    InputAuthor.setAttribute("required", "");
+    bookForm.appendChild(InputAuthor);
+
+    const LabelNOP = document.createElement("label");
+    LabelNOP.setAttribute("for", "NOP");
+    LabelNOP.textContent = "Number Of Pages: ";
+    bookForm.appendChild(LabelNOP);
+    const InputNOP = document.createElement("input");
+    InputNOP.setAttribute("type", "number");
+    InputNOP.setAttribute("id", "NOP");
+    InputNOP.setAttribute("required", "");
+    bookForm.appendChild(InputNOP);
+
+    const fieldSet = document.createElement("fieldset");
+    bookForm.appendChild(fieldSet);
+    const legend = document.createElement("legend");
+    legend.textContent = "Status: ";
+    fieldSet.appendChild(legend);
+
+    const radioList = document.createElement("ul");
+    fieldSet.appendChild(radioList);
+    const listOne = document.createElement("li");
+    const listTwo = document.createElement("li");
+    const listThree = document.createElement("li");
+    radioList.appendChild(listOne);
+    radioList.appendChild(listTwo);
+    radioList.appendChild(listThree);
+
+    const LabelStatusOne = document.createElement("label");
+    LabelStatusOne.setAttribute("for", "notRead");
+    LabelStatusOne.textContent = "not read ";
+    listOne.appendChild(LabelStatusOne);
+    const InputStatusOne = document.createElement("input");
+    InputStatusOne.setAttribute("type", "radio");
+    InputStatusOne.setAttribute("id", "notRead");
+    listOne.appendChild(InputStatusOne);
+
+    const LabelStatusTwo = document.createElement("label");
+    LabelStatusTwo.setAttribute("for", "Read");
+    LabelStatusTwo.textContent = "read ";
+    listTwo.appendChild(LabelStatusTwo);
+    const InputStatusTwo = document.createElement("input");
+    InputStatusTwo.setAttribute("type", "radio");
+    InputStatusTwo.setAttribute("id", "Read");
+    listTwo.appendChild(InputStatusTwo);
+
+    const LabelStatusThree = document.createElement("label");
+    LabelStatusThree.setAttribute("for", "dropped");
+    LabelStatusThree.textContent = "dropped ";
+    listThree.appendChild(LabelStatusThree);
+    const InputStatusThree = document.createElement("input");
+    InputStatusThree.setAttribute("type", "radio");
+    InputStatusThree.setAttribute("id", "dropped");
+    listThree.appendChild(InputStatusThree);
 
 
-newBook.addEventListener("click", () => console.log(this))
+}
+
+openForm()
